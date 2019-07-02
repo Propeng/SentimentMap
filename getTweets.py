44fd94dd96deec16975b5c59b5367fe15f2301c4
Lujine Elfeky <lujineelfeky@gmail.com>
2019-07-02 10:19:19 +0200

    Added basic script to retrive tweets

---
+++ getTweets.py
@@ -0,0 +1,33 @@
+# Import the Twython class
+from twython import Twython  
+import json
+import pandas as pd
+
+# Load credentials from json file
+with open("credentials.json", "r") as file:  
+    creds = json.load(file)
+
+# Instantiate an object
+python_tweets = Twython(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
+
+# Create our query
+query = {'geocode':'29.988152,31.419053,10km',  
+        'result_type': 'recent',
+        'count': 10,
+        'lang': 'en',
+        }
+
+# Search tweets
+dict_ = {'user': [], 'date': [], 'text': [], 'favorite_count': []}  
+for status in python_tweets.search(**query)['statuses']:  
+    dict_['user'].append(status['user']['screen_name'])
+    dict_['date'].append(status['created_at'])
+    dict_['text'].append(status['text'])
+    dict_['favorite_count'].append(status['favorite_count'])
+print(dict_)
+
+# Structure data in a pandas DataFrame for easier manipulation
+df = pd.DataFrame(dict_)  
+df.sort_values(by='favorite_count', inplace=True, ascending=False)  
+df.head(5)  
+print('done')
