ff467c5de624322ee29bee501aef8cd948e95a5b
propeng <aa.mahdawy.10@gmail.com>
2019-07-02 12:03:48 +0200

    Find tweets for each region

--- getTweets.py
+++ getTweets.py
@@ -2,8 +2,14 @@
 from twython import Twython  
 import json
 import pandas as pd
+from datetime import date, timedelta
 
-regions = [('New Cairo', '29.988152,31.419053,10km')]
+regions = [
+    {'name': 'New Cairo', 'geocode': '30.028652,31.465498,10km', 'tweets': []},
+    {'name': 'Dokki', 'geocode': '30.039874,31.206894,3km', 'tweets': []},
+    {'name': 'Heliopolis', 'geocode': '30.100987,31.342999,5km', 'tweets': []},
+    {'name': 'Abbaseya', 'geocode': '30.065151,31.275649,3km', 'tweets': []},
+]
 
 # Load credentials from json file
 with open("credentials.json", "r") as file:  
@@ -20,19 +26,38 @@ query = {'result_type': 'recent',
 
 # Search tweets
 #dict_ = {'user': [], 'date': [], 'text': [], 'region': []}
-tweets = []
-for (region_name, geocode) in regions:
-    query['geocode'] = geocode
-    for status in python_tweets.search(**query)['statuses']:
-        tweet = {}
-        tweet['user'] = status['user']['screen_name']
-        tweet['date'] = status['created_at']
-        tweet['text'] = status['text']
-        tweet['region'] = region_name
-        tweets.append(tweet)
+for day_i in range(7):
+    for region in regions:
+        query['geocode'] = region['geocode']
+        query['until'] = str(date.today() - timedelta(days=day_i))
+        query['since'] = str(date.today() - timedelta(days=day_i+1))
+        print(query)
+        print()
+        query_result = python_tweets.search(**query)
 
-with open('tweets.json', 'w') as tweets_file:
-    json.dump(tweets, tweets_file, indent=4)
+        for status in query_result['statuses']:
+            tweet = {}
+            tweet['id'] = status['id']
+            tweet['user'] = status['user']['screen_name']
+            tweet['user_location'] = status['user']['location']
+            tweet['date'] = status['created_at']
+            tweet['text'] = status['text']
+            tweet['geo'] = status['geo']
+            try:
+                tweet['bounding_box'] = status['bounding_box']
+            except KeyError:
+                pass
+            tweet['lang'] = status['lang']
+            tweet['urls'] = status['entities']['urls']
+            tweet['mentions'] = status['entities']['user_mentions']
+            tweet['region'] = region['name']
+            region['tweets'].append(tweet)
+
+# Write tweets to JSON files
+for region in regions:
+    print(region['name'], len(region['tweets']))
+    with open('tweets/'+region['name']+'.json', 'w') as tweets_file:
+        json.dump(region['tweets'], tweets_file, indent=4)
 
 # Structure data in a pandas DataFrame for easier manipulation
 #df = pd.DataFrame(dict_)
