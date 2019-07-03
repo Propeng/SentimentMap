11c89daf535aaa46b6ee57fc73b4a03f251fc27e
propeng <aa.mahdawy.10@gmail.com>
2019-07-03 14:37:29 +0200

    Load training data CSV

--- filterTraining.py
+++ filterTraining.py
@@ -1,1 +1,14 @@
+import pandas as pd
 import csv
+
+import utils
+
+with open("trainingdata.csv", "r") as csv_file:
+    reader = csv.DictReader(csv_file, fieldnames=["polarity", "id", "date", "query", "username", "text"])
+    data = [row for row in reader]
+
+df = pd.DataFrame(data)
+df.set_index('id', drop = True, inplace=True)
+
+#df['filtered_text'] = df.apply(utils.process, axis=1)
+df.to_pickle("./training.pkl")
