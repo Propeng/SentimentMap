1f4854aa4fd8cbbfe6b13a37e8b9b46b4dd96bf5
Lujine Elfeky <lujineelfeky@gmail.com>
2019-07-04 14:32:28 +0200

    Added code to run the second classifier

--- classifier1.py
+++ classifier1.py
@@ -9,45 +9,56 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import nltk.classify.util
 from nltk.classify import NaiveBayesClassifier
+import pickle
 
-twitter_samples.fileids()
- 
+
 def create_word_features(words):
     useful_words = [word for word in words if word not in stopwords.words("english")]
     my_dict = dict([(word, True) for word in useful_words])
     return my_dict
 
-create_word_features(["the", "quick", "brown", "fox", "fox"])
-
-strings = twitter_samples.strings('negative_tweets.json')
-neg_reviews = []
-for tweet in strings:
-    tweet = tweet.replace(":", "").replace("(", "").replace(")", "")
-    feature = create_word_features(word_tokenize(tweet))
-    print(feature)
-    neg_reviews.append((feature, "negative"))
-   
-strings = twitter_samples.strings('positive_tweets.json')   
-pos_reviews = []
-for tweet in strings:
-    tweet = tweet.replace(":", "").replace("(", "").replace(")", "")
-    feature = create_word_features(word_tokenize(tweet))
-    print(feature)
-    pos_reviews.append((feature, "positive"))
+if __name__ == "__main__":
     
-print(len(neg_reviews))
-print(len(pos_reviews))
-print(neg_reviews[:5])
+    twitter_samples.fileids()
+    
+    create_word_features(["the", "quick", "brown", "fox", "fox"])
+    
+    strings = twitter_samples.strings('negative_tweets.json')
+    neg_reviews = []
+    for tweet in strings:
+        tweet = tweet.replace(":", "").replace("(", "").replace(")", "")
+        feature = create_word_features(word_tokenize(tweet))
+        print(feature)
+        neg_reviews.append((feature, "negative"))
+       
+    strings = twitter_samples.strings('positive_tweets.json')   
+    pos_reviews = []
+    for tweet in strings:
+        tweet = tweet.replace(":", "").replace("(", "").replace(")", "")
+        feature = create_word_features(word_tokenize(tweet))
+        print(feature)
+        pos_reviews.append((feature, "positive"))
+        
+    print(len(neg_reviews))
+    print(len(pos_reviews))
+    print(neg_reviews[:5])
+    
+    train_set = neg_reviews[:4000] + pos_reviews[:4000]
+    test_set =  neg_reviews[0:1000] + pos_reviews[0:1000]
+    print(len(train_set),  len(test_set))
+    
+    
+    
+    classifier = NaiveBayesClassifier.train(train_set)
+    accuracy = nltk.classify.util.accuracy(classifier, test_set)
+    print(accuracy)
+    
+    
+    for tweet in test_set:
+        print((tweet,classifier.classify(tweet[0])))
+        
+    with open('second_classifier.pkl', 'wb') as classifier_file:
+        pickle.dump(classifier, classifier_file)
 
-train_set = neg_reviews[:4000] + pos_reviews[:4000]
-test_set =  neg_reviews[0:1000] + pos_reviews[0:1000]
-print(len(train_set),  len(test_set))
-
-
-
-classifier = NaiveBayesClassifier.train(train_set)
-accuracy = nltk.classify.util.accuracy(classifier, test_set)
-
-print(accuracy)
 
 
--- runClassifier.py
+++ runClassifier.py
@@ -1,17 +1,31 @@
 import pickle
 import pandas as pd
+import classifier1
 
+# first classifier
 with open('analyzer.pkl', 'rb') as analyzer_file:
     analyzer = pickle.load(analyzer_file)
 with open('classifier.pkl', 'rb') as classifier_file:
     classifier = pickle.load(classifier_file)
+
+# second classifier
+with open('second_classifier.pkl', 'rb') as classifier_file:
+    classifier2 = pickle.load(classifier_file)
 
 def assign_sentiment(tweet):
     test_set = analyzer.apply_features([(tweet['filtered_text'], '')])
     prob = classifier.prob_classify(test_set[0][0])
     return prob.prob('pos')
 
+def assign_sentiment2(tweet):
+    test_set = classifier1.create_word_features(tweet['filtered_text'])
+    prob = classifier2.prob_classify(test_set)
+    return (prob.prob('positive')) 
+
 df = pd.read_pickle('tweets.pkl')
 df['sentiment'] = df.apply(assign_sentiment, axis=1)
+df['sentiment2'] = df.apply(assign_sentiment2, axis=1)
 
 df.to_pickle('tweets.pkl')
+
+
