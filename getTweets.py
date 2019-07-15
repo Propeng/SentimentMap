3d657dd4a75c3ed21ee196972abc19a345163248
propeng <aa.mahdawy.10@gmail.com>
2019-07-15 13:58:45 +0200

    Plotting

--- getTweets.py
+++ getTweets.py
@@ -5,17 +5,11 @@ import pandas as pd
 from datetime import date, timedelta
 
 import utils
+from regions import regions
 
 print("Tweets will be fetched for the days", str(date.today() - timedelta(days=7)),
     "to", str(date.today()))
 print()
-
-regions = [
-    {'name': 'New Cairo', 'geocode': '30.028652,31.465498,10km', 'tweets': {}},
-    {'name': 'Dokki', 'geocode': '30.039874,31.206894,3km', 'tweets': {}},
-    {'name': 'Heliopolis', 'geocode': '30.100987,31.342999,5km', 'tweets': {}},
-    {'name': 'Abbaseya', 'geocode': '30.065151,31.275649,3km', 'tweets': {}},
-]
 
 # Load credentials from json file
 with open("credentials.json", "r") as file:  
---
+++ regions.py
@@ -0,0 +1,6 @@
+regions = [
+    {'name': 'New Cairo', 'geocode': '30.028652,31.465498,10km', 'tweets': {}},
+    {'name': 'Dokki', 'geocode': '30.039874,31.206894,3km', 'tweets': {}},
+    {'name': 'Heliopolis', 'geocode': '30.100987,31.342999,5km', 'tweets': {}},
+    {'name': 'Abbaseya', 'geocode': '30.065151,31.275649,3km', 'tweets': {}},
+]
